f72abee7020f886c926ade541ff8cc1d1e44a9db
rosemo21 <rosemolpp2001@gmail.com>
2024-10-29 10:04:42 +0530

    assignment2 answers

--- assignment1.js
+++ assignment1.js
@@ -15,21 +15,23 @@ let variable7 = null;
 console.log("variable7 is", typeof variable7, "type");
 let variable8
 console.log("variable8 is", typeof variable8, "type");
+
 //Write a JS program to show an alert message on the loading of the website.
 alert("hello welcome to my page");
+
 //["1", "2", "3", "4", "5", "6", "7"]
 // Remove number "6" from the array and console the length of the array.
 let arr = ["1", "2", "3", "4", "5", "6", "7"];
 arr.splice(arr.indexOf("6"), 1);
 console.log(arr);
 console.log(arr.length);
+
 //Convert all the items of the array to data type number and console each items data type, use any of the array iteration methods provided by JS for iteration.
 let arr2 = ["1", "2", "3", "4", "5", "6", "7"];
-for (let i = 0; i <= arr2.length - 1; i++) {
-    arr2[i] = Number(arr2[i]);
-    console.log(typeof arr2[i]);
-}
+arr2=arr2.map(arrayelement=>Number(arrayelement));
+arr2.forEach(arrayelement=>console.log(typeof arrayelement));
 console.log(arr2);
+
 //Remove last three items of the array, use JS provided array method, then console the array and then add "one" and "two" (strings) to the beginning of the array and console the array. 
 let arr3 = ["1", "2", "3", "4", "5", "6", "7"];
 arr3.splice(arr3.length - 3, 3);
@@ -37,53 +39,54 @@ console.log(arr3.length);
 console.log(arr3);
 arr3.splice(0, -2, "one", "two");
 console.log(arr3);
+
 //Using any one of the array iteration methods console the string concatenation of all items of the array and also console the sum of all the items ( convert to integer before calculating)
 let arr4 = ["1", "2", "3", "4", "5", "6", "7"];
-var sum = 0;
-for (let i = 1; i <= arr4.length - 1; i++) {
-    if (i == 1) {
-        var k = arr4[i - 1] + arr4[i];
-    }
-    else {
-        k = k + arr4[i];
-    }
+let sum=0;
+let concatenatedstring="";
+arr4.forEach(function(number){
+  sum=Number(number)+sum;
+  concatenatedstring=concatenatedstring+number;
+})
+console.log(sum,"sum of all the items ");
+console.log(concatenatedstring,"is the final string");
 
-    sum = Number(arr4[i]) + sum;
-}
-console.log(k, "is the final string got by concatenating all the elements in the array");
-console.log(sum, "is the sum of elements in given array");
 //Filter out item "3" from the array and console the array (use array method)
 let arr5 = ["1", "2", "3", "4", "5", "6", "7"];
 arr5 = arr5.filter(item => item != "3");
 console.log(arr5);
+
 //Iterate the array and console the item, when item is either "3", "6" or "7"
 let arr6 = ["1", "2", "3", "4", "5", "6", "7"];
-for (let i = 0; i <= arr6.length - 1; i++) {
-    if (arr6[i] == "3" || arr6[i] == "6" || arr6[i] == "7") {
-        console.log(arr6[i]);
+arr6.forEach((value)=>{
+    if(value=="3"||value=="6"||value=="7"){
+        console.log("value",value,"is present inside this array");
     }
-}
+})
+
 //[1, 2, "3", 4, 5, 6,"7"]  Compare this array with the above given array and console only if both items of the array have same data type. (Compare each item of this array with each item of the other array)
 let arr7 = [1, 2, "3", 4, 5, 6, "7"];
-for (i = 0; i <= arr6.length - 1; i++) {
-    for (j = 0; j <= arr7.length - 1; j++) {
-        if (typeof arr6[i] == typeof arr7[j]) {
-            console.log("element", arr6[i], "with indexnumber", i, "and", "element", arr7[j], "with indexnumber", j, " are of same datatype")
+arr6.forEach((value1,index1)=>{
+    arr7.forEach((value2,index2)=>{
+        if(typeof value1==typeof value2){
+            console.log(value1,"of index",index1,"and",value2,"of index",index2,"are of same datatype");
         }
-    }
-}
+    })
+})
+
 //[0,2,3,7,5,6,8] iterate the array and multiply each item by its index value and console the result only if result is greater than 40.
 let arr8 = [0, 2, 3, 7, 5, 6, 8];
-for (i = 0; i <= arr8.length - 1; i++) {
-    let k = arr8[i] * i;
-    if (k == 40) {
-        console.log("result is equal to 40");
+arr8.forEach((value, index) => {
+    let product = value * index;
+    if (product === 40) {
+        console.log(value,"*",index,"result is equal to 40");
+    } else {
+        console.log(product);
+        console.log(value, "*", index, "not equal to 40");
     }
-    else {
-        console.log(k);
-        console.log(arr8[i], "*", i, "not equal to 40");
-    }
-}
+});
+
+
 //Create two arrays with five items each and merge the array into a single array and then console it.
 let arr9 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 let arr10 = ["a", "b", "c", "d", "e", "f"];
