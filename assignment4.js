7430e4a0ab8866811f0dabdade8fcaeb117f6999
rosemo21 <rosemolpp2001@gmail.com>
2024-11-12 10:10:58 +0530

    assignment 4 resubmission

--- assignment4.js
+++ assignment4.js
@@ -24,7 +24,6 @@ document.getElementById("show-details-button").onclick = function () {
 };
 
 // Create a select box with numbers 1 to 10,  when selected 9, you should change the selection to 10 and show a message that "9 is fully occupied please select another number", when selected any number other than 9 it should show a message as "you selected 'particular number' " in a div, on hovering the div it should change the background color of the div into a highlighting shade, while the mouse pointer leaves the message area the background color should go back to as before (don't use CSS to attain the hovering functionality)
-
 document.getElementById('first-select-box').onchange = function () {
     var selectBox = document.getElementById('first-select-box');
     if (selectBox.value == '9') {
@@ -64,51 +63,52 @@ programs.forEach((value, index) => {
 // pin code and mobile number fields should not be submitted with non-integer values, if so, then show an error msg stating only numbers are allowed.
 // Minimum length of phone number should be 10, otherwise show corresponding error msg below the mobile no. field. 
 // Make a prepopulate button, which when clicked will populate the form with values in the local storage if it exists, otherwise the button will be disabled.
+document.getElementById("prepopulate").disabled = true;
 document.getElementById("form-details").onsubmit = function (event) {
-    let formname = document.getElementById("name").value;
-    let phonenumber = document.getElementById("phoneno").value;
-    let placename = document.getElementById("place").value;
-    let nameofcompany = document.getElementById("companyname").value;
-    let pinnumber = document.getElementById("pincode").value;
-    let text = "";
+    event.preventDefault();
+    let formname = document.getElementById("name").value.trim();
+    let phonenumber = document.getElementById("phoneno").value.trim();
+    let placename = document.getElementById("place").value.trim();
+    let nameofcompany = document.getElementById("companyname").value.trim();
+    let pinnumber = document.getElementById("pincode").value.trim();
+    let errormsgarray = ["name-error", "phoneno-error", "place-error", "companyname-error", "pincode-error"];
     let arrayformelements = [formname, phonenumber, placename, nameofcompany, pinnumber];
     let arrayofnumberelements = [phonenumber, pinnumber];
+    let Valid = true;
+    errormsgarray.forEach(errorarryelement => document.getElementById(errorarryelement).innerHTML = "");
     arrayformelements.forEach((arrayelement, index) => {
         if (arrayelement == "") {
-            text = "error:this field is required";
-            event.preventDefault();
-            let errormsgarray = ["name-error", "phoneno-error", "place-error", "companyname-error", "pincode-error"];
-            document.getElementById(errormsgarray[index]).innerHTML = text;
+            document.getElementById(errormsgarray[index]).innerHTML = "error: this field is required";
+            Valid = false;
         }
-        else {
-            arrayofnumberelements.forEach((numberelement, index) => {
-                if (isNaN(numberelement)) {
-                    event.preventDefault();
-                    let errormsgarray = ["phoneno-error", "pincode-error"];
-                    document.getElementById(errormsgarray[index]).innerHTML = "error : only numbers are allowed";
-                }
-                else if (index == 0 && numberelement.length < 10) {
-                    let errormsgarray = ["phoneno-error", "pincode-error"]
-                    event.preventDefault();
-                    document.getElementById(errormsgarray[index]).innerHTML = "Minimum length of phone number should be 10";
-                }
-                else if (index == 1 && numberelement.length < 6) {
-                    let errormsgarray = ["phoneno-error", "pincode-error"]
-                    event.preventDefault();
-                    document.getElementById(errormsgarray[index]).innerHTML = "Minimum length of Pin code should be 6";
-                }
-                else {
-                    localStorage.setItem("fullname", formname);
-                    localStorage.setItem("phone number", phonenumber);
-                    localStorage.setItem("place name", placename);
-                    localStorage.setItem("company name", nameofcompany);
-                    localStorage.setItem("pin code", pinnumber);
-                }
-
-            })
-
+    });
+    if (!isNaN(phonenumber)) {
+        if (phonenumber.length < 10) {
+            document.getElementById("phoneno-error").innerHTML = "Minimum length of phone number should be 10";
+            Valid = false;
         }
-    })
+    } else {
+        document.getElementById("phoneno-error").innerHTML = "error: only numbers are allowed";
+        Valid = false;
+    }
+    if (!isNaN(pinnumber)) {
+        if (pinnumber.length < 6) {
+            document.getElementById("pincode-error").innerHTML = "Minimum length of Pin code should be 6";
+            Valid = false;
+        }
+    } else {
+        document.getElementById("pincode-error").innerHTML = "error: only numbers are allowed";
+        Valid = false;
+    }
+    if (Valid) {
+        localStorage.setItem("fullname", formname);
+        localStorage.setItem("phone number", phonenumber);
+        localStorage.setItem("place name", placename);
+        localStorage.setItem("company name", nameofcompany);
+        localStorage.setItem("pin code", pinnumber);
+        document.getElementById("prepopulate").disabled = false;
+        document.getElementById("form-details").reset();
+    }
 };
 document.getElementById("prepopulate").onclick = function (event) {
     event.preventDefault();
@@ -117,8 +117,7 @@ document.getElementById("prepopulate").onclick = function (event) {
     document.getElementById("place").value = localStorage.getItem("place name");
     document.getElementById("companyname").value = localStorage.getItem("company name");
     document.getElementById("pincode").value = localStorage.getItem("pin code");
-
-}
+};
 
 // Create a form with a text field which when submitted, will change the tab title to whatever is entered, 
 // limit the field to 50 characters, otherwise show error message, stay on the same page when submitted(it shouldn't refresh).
